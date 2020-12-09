28d7a0e3c20c42b4cff0bb67767404f765302ac2
Saksham Bejwani <sakshambejwani@gmail.com>
2020-12-09 20:02:55 +0530

    Attached postgres to backend

--- firstapp/firstapp/settings.py
+++ firstapp/firstapp/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'firstapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'lco-firstapp',
+        'USER': 'postgres',
+        'PASSWORD': '123',
+        'HOST': 'localhost'
     }
 }
 
